22a585df47902f9ac3efa40d947e0900dcb66cf9
Joe Moon <joe@xoxomoon.com>
2015-11-30 00:04:01 -0800

    clean up syntax

--- src/index.js
+++ src/index.js
@@ -8,52 +8,64 @@ import isUndefined from 'lodash.isundefined';
 
 export const assign = (...args) => _assign({}, ...args);
 
+export const separateStateAndXforms = tree => reduce(tree, (memo, val, key) => {
+  if (isFunction(val)) {
+    memo.xforms[key] = val;
+    return memo;
+  }
+  if (isPlainObject(val)) {
+    let { state, xforms } = separateStateAndXforms(val);
+    memo.state[key] = state;
+    if (!isEmpty(xforms)) {
+      memo.xforms[key] = xforms;
+    }
+    return memo;
+  }
+  memo.state[key] = val;
+  return memo;
+}, {
+  state: {},
+  xforms: {}
+});
+
+export const createActionCreatorTree = (tree, path = []) => reduce(tree, (memo, val, key) => _assign(memo, {
+  [key]: isFunction(val) ? payload => ({
+    type: path.concat(key).join('.'),
+    payload
+  }) : createActionCreatorTree(val, path.concat(key))
+}), {});
+
+export const bindActionCreatorTree = (tree, dispatch, path = []) => reduce(tree, (memo, val, key) => _assign(memo, {
+  [key]: isFunction(val)
+    ? payload => dispatch(tree[key](payload))
+    : bindActionCreatorTree(val, dispatch, path.concat(key))
+}), {});
+
+const createReducerTree = (tree, path = []) => reduce(tree, (memo, val, key) => {
+  const currentPath = path.concat(key);
+  const actionType = currentPath.join('.');
+  return _assign(memo, {
+    [key]: isFunction(val) ? (state, {type, payload}) => {
+      if (type !== actionType) {
+        return state;
+      }
+      if(isUndefined(state)) {
+        return state;
+      }
+      return val(state, payload);
+    } : createReducerTree(val, currentPath)
+  });
+}, {});
+
+const recursiveCombineReducers = tree => (state, action) => {
+  return reduce(tree, (memo = {}, val, key) => _assign(memo, isFunction(val) ? val(memo, action) : {
+    [key]: recursiveCombineReducers(val)(memo[key], action)
+  }), state);
+};
+
 export const createReducer = compose(recursiveCombineReducers, createReducerTree);
 
-export function separateStateAndXforms(tree) {
-	return reduce(tree, (memo, val, key) => {
-		if (isFunction(val)) {
-			memo.xforms[key] = val;
-			return memo;
-		}
-		if (isPlainObject(val)) {
-			let { state, xforms } = separateStateAndXforms(val);
-			memo.state[key] = state;
-			if (!isEmpty(xforms)) {
-				memo.xforms[key] = xforms;
-			}
-			return memo;
-		}
-		memo.state[key] = val;
-		return memo;
-	}, {
-		state: {},
-		xforms: {}
-	});
-}
-
-export function createActionCreatorTree(tree, path = []) {
-	return reduce(tree, (memo, val, key) => {
-		memo[key] = isFunction(val) ? (payload) => {
-			return {
-				type: path.concat(key).join('.'),
-				payload
-			};
-		} : createActionCreatorTree(val, path.concat(key));
-		return memo;
-	}, {});
-}
-
-export function bindActionCreatorTree(tree, dispatch, path = []) {
-  return reduce(tree, (memo, val, key) => {
-    memo[key] = isFunction(val) ?
-      payload => dispatch(tree[key](payload)) :
-      bindActionCreatorTree(val, dispatch, path.concat(key));
-    return memo;
-  }, {});
-}
-
-export default function (tree) {
+export const rehash = tree => {
 	const { state, xforms } = separateStateAndXforms(tree);
   const actionCreatorTree = createActionCreatorTree(xforms);
 	return {
@@ -63,25 +75,4 @@ export default function (tree) {
 	};
 }
 
-function createReducerTree(tree, path = []) {
-	return reduce(tree, (memo, val, key) => {
-		let currentPath = path.concat(key);
-		memo[key] = isFunction(val) ? (state, {type, payload}) => {
-			if (type === currentPath.join('.') && !isUndefined(state)) {
-				return val(state, payload);
-			}
-			return state;
-		} : createReducerTree(val, currentPath);
-		return memo;
-	}, {});
-}
-
-function recursiveCombineReducers(tree) {
-	return (state, action) => {
-		return reduce(tree, (memo = {}, val, key) => {
-      return assign(memo, isFunction(val) ? val(memo, action) : {
-        [key]: recursiveCombineReducers(val)(memo[key], action)
-      });
-		}, state);
-	};
-}
+export default rehash;
